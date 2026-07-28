be085e743d30ae2ca7853f9518eea28e94b96e6e
diademma <77641647+diademma@users.noreply.github.com>
2026-07-28 17:27:07 +0300

    Delete app/src/main/java/com/heiher/hev/socks5/tunnel/HevSocks5TunnelService.java

--- app/src/main/java/com/heiher/hev/socks5/tunnel/HevSocks5TunnelService.java
+++
@@ -1,7 +0,0 @@
-package com.heiher.hev.socks5.tunnel;
-
-public class HevSocks5TunnelService {
-    // Вторая заглушка для C++ библиотеки
-    public static void HevSocks5TunnelCallBack(int code) {
-    }
-}
