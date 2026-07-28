8e5349439352c14f8f5ff0c295d1658153ab30e3
diademma <77641647+diademma@users.noreply.github.com>
2026-07-28 17:14:50 +0300

    Create HevSocks5TunnelService.java

---
+++ app/src/main/java/com/heiher/hev/socks5/tunnel/HevSocks5TunnelService.java
@@ -0,0 +1,7 @@
+package com.heiher.hev.socks5.tunnel;
+
+public class HevSocks5TunnelService {
+    // Вторая заглушка для C++ библиотеки
+    public static void HevSocks5TunnelCallBack(int code) {
+    }
+}
